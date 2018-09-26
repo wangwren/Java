e61c7681bb71b33424fb6be93d310805ce23bac5
王威人 <291762380@qq.com>
2018-09-26 23:13:26 +0800

    练习

---
+++ Chapter13Exercise/src/strings/ScannerDelimiter.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ScannerDelimiter {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner("12,42,78,99,42");
+		sc.useDelimiter("\\s*,\\s*");
+		while(sc.hasNextInt()) {
+			System.out.println(sc.nextInt());
+		}
+	}
+}
